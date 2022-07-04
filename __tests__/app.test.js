9f6ae5634d4a64ddc9a2bec4e6cc2d040609791b
Henry Grainger <henry.grainger@evri.com>
2022-07-04 15:51:34 +0100

    completed getbyId request for articles and added a 404 test to the test file

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -39,3 +39,31 @@ describe("1. GET /api/topics", () => {
           });
       });
     });
+
+    describe("2. GET /api/articles/:article_id", () => {
+        test('status:200, responds with the article with the corresponding id', () => {
+            const ARTICLE_ID = 2;
+            return request(app)
+              .get(`/api/articles/${ARTICLE_ID}`)
+              .expect(200)
+              .then(({ body }) => {
+                expect(body.article).toEqual({
+                  article_id: ARTICLE_ID,
+                  author: 'icellusedkars',
+                  title: 'Sony Vaio; or, The Laptop',
+                  body: "Call me Mitchell. Some years ago—never mind how long precisely—having little or no money in my purse, and nothing particular to interest me on shore, I thought I would buy a laptop about a little and see the codey part of the world. It is a way I have of driving off the spleen and regulating the circulation. Whenever I find myself growing grim about the mouth; whenever it is a damp, drizzly November in my soul; whenever I find myself involuntarily pausing before coffin warehouses, and bringing up the rear of every funeral I meet; and especially whenever my hypos get such an upper hand of me, that it requires a strong moral principle to prevent me from deliberately stepping into the street, and methodically knocking people’s hats off—then, I account it high time to get to coding as soon as I can. This is my substitute for pistol and ball. With a philosophical flourish Cato throws himself upon his sword; I quietly take to the laptop. There is nothing surprising in this. If they but knew it, almost all men in their degree, some time or other, cherish very nearly the same feelings towards the the Vaio with me.",
+                  created_at: '2020-10-16T05:03:00.000Z',
+                  topic: "mitch",
+                  votes: 0
+});
+              });
+          });
+          test('status:400, responds with an error message when passed a bad user ID', () => {
+            return request(app)
+              .get('/api/articles/notAnId')
+              .expect(400)
+              .then(({ body }) => {
+                expect(body.msg).toBe('Invalid input');
+              });
+          });
+    })
--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 const express = require("express");
-const {getTopics} = require('./controllers/getTopics');
+const { getTopics } = require('./controllers/getTopics');
+const { getArticleById } = require('./controllers/getArticleById');
 
 const app = express();
 app.use(express.json());
 
 app.get("/api/topics", getTopics);
+app.get("/api/articles/:article_id", getArticleById);
+
+
+app.use((err, req, res, next) => {
+    if (err.code === '22P02') {
+      res.status(400).send({ msg: 'Invalid input' });
+    }});
+  
 
 
 app.use('*', (req, res) => {
