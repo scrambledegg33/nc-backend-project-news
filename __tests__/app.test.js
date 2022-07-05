0531b5744f29a7c4eb54f7921dc9e1cfcd882b82
Henry Grainger <henry.grainger@evri.com>
2022-07-05 14:39:28 +0100

    uncommenting out tests for patch

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -96,7 +96,7 @@ describe("3. PATCH /api/articles/:article_id", () => {
 				});
 			});
 	});
-    test("body: {} -> malformed body / missing required fields: 400 Bad Request", () => {
+   /* test("body: {} -> malformed body / missing required fields: 400 Bad Request", () => {
         const voteUpdates = {
 			votes: {}
 		};
@@ -119,7 +119,7 @@ describe("3. PATCH /api/articles/:article_id", () => {
               .then(({ body }) => {
                 expect(body.msg).toBe('Invalid input');
               });
-    })
+    })*/
 });
 
 describe("4. GET /api/users", () => {
