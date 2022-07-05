b2ba73981df6929418e047edad1fc0fae47d0d63
Henry Grainger <henry.grainger@evri.com>
2022-07-05 15:06:11 +0100

    completed 400 tests and patch request

--- models/patchArticle.js
+++ models/patchArticle.js
@@ -1,16 +1,17 @@
 const db = require('../db/connection.js');
 
 const patchArticle = (votes, articleId) => {
-    return db.query(`UPDATE articles SET votes = (votes + $1) WHERE article_id = $2 RETURNING *;`, [votes, articleId])
+   
+ 
+  if(!Number.isInteger(votes)){
+    return Promise.reject({status: 400, msg: 'Invalid input'});
+  }
+  
+  
+  return db.query(`UPDATE articles SET votes = (votes + $1) WHERE article_id = $2 RETURNING *;`, [votes, articleId])
     .then(({ rows }) => {
-        const articleInfo = rows[0];
-        if (articleInfo === {}) {
-          return Promise.reject({
-            status: 400,
-            msg: 'Invalid input',
-          });
-        }
-        return articleInfo;
+       
+        return rows[0];
       })
     .catch((err) => {
         console.log(err);
