1b7e03ead99dc641ff3004d223cc2237acf6c1cf
Henry Grainger <henry.grainger@evri.com>
2022-07-08 10:54:17 +0100

    finsished endpoint.json file and started get request

---
+++ controllers/getDescription.js
@@ -0,0 +1,10 @@
+
+
+/*const getDescription = (req, res, next) => {
+    res.status(200).send(endpoints.json).catch((err) => {
+        next(err);
+    });
+}
+
+
+module.exports = getDescription;*/
