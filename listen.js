78282ea2e195c90e411ede9f9aed085032aa794b
Henry Grainger <henry.grainger@gmail.com>
2022-09-23 10:54:54 +0100

    pushing changes to host on heroku

--- listen.js
+++ listen.js
@@ -1,7 +1,8 @@
 const app = require("./app.js");
 
-const PORT = 9090;
+const { PORT = 9090 } = process.env;
 
 app.listen(PORT, () => {
-    console.log(`app listening on port ${PORT}`)
-})
+  console.log(`Listening on ${PORT}...`)
+  }
+);
